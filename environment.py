be690297579e56ef9193ad0a03bde8f2a97044e4
Mikhail <AiNo@Mikhails-MacBook-Air.local>
2016-11-12 15:02:22 +0300

    Fix pep8 and convert tabs to spaces and ix error with browser detection

--- environment.py
+++ environment.py
@@ -6,17 +6,19 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from settings import settings_test as settings
-from settings import download_folder_path
 from os import getcwd
-#from api_method import delete_data
+# from api_method import delete_data
 from sys import platform
 import time
 import os
 '''
 Файл environment.py устанавливает верхний слой окружения для behave.
-Тут можно объявить переменные, которые можно будет использовать на всех уровнях,
+Тут можно объявить переменные,
+которые можно будет использовать на всех уровнях,
 выполнения тестов. (Например, на уровне фичи, сценария, конкретного шага.)
-Так же тут можно объявить функции, которые будут выполняться после шагов, сценариев, тэгов или фич.
+Так же тут можно объявить функции,
+которые будут выполняться после шагов,
+сценариев, тэгов или фич.
 Узнай больше на http://pythonhosted.org/behave/.
 '''
 
@@ -26,6 +28,7 @@ LOGIN = settings['login']
 PASSWORD = settings['password']
 STAND = settings['stand_number']
 
+
 def sleep_while_show_text(context, text):
     '''
     Функция ставит выполнение на паузу, пока на экране есть указанный текст.
@@ -34,11 +37,13 @@ def sleep_while_show_text(context, text):
     '''
     while True:
         try:
-           context.browser.find_element_by_xpath('//*[contains(text(), "%s")]')
+            context.browser.find_element_by_xpath(
+                '//*[contains(text(), "%s")]')
         except:
             # print ('Блок загрузки не найден!')
             return 1
         time.sleep(1)
+
 
 def login(context, server=SERVER, login=LOGIN, password=PASSWORD):
     '''
@@ -48,7 +53,7 @@ def login(context, server=SERVER, login=LOGIN, password=PASSWORD):
     # неявные ожидания
     # context.browser.implicitly_wait(120)
     print ('I try to login into account...')
-    
+
     context.browser.get(server)
     context.browser.find_element_by_link_text("Login").click()
     WebDriverWait(context.browser, 1000).until(
@@ -57,48 +62,54 @@ def login(context, server=SERVER, login=LOGIN, password=PASSWORD):
     context.browser.find_element_by_id("RegisterForm_email").clear()
     context.browser.find_element_by_id("RegisterForm_email").send_keys(login)
     context.browser.find_element_by_id("RegisterForm_password").clear()
-    context.browser.find_element_by_id("RegisterForm_password").send_keys(password)
+    context.browser.find_element_by_id(
+        "RegisterForm_password").send_keys(password)
     context.browser.find_element_by_name("yt0").click()
-       
-    text = 'All the Icons You Need. Guaranteed.'.decode('utf-8')
+
+    text = 'All the Icons You Need. Guaranteed.'
+    text = text.decode('utf-8')
     WebDriverWait(context.browser, 1000).until(
-        EC.presence_of_element_located((By.XPATH, "//*[@id='home-app']/div[2]/div/h2"))
+        EC.presence_of_element_located(
+            (By.XPATH, "//*[@id='home-app']/div[2]/div/h2"))
     )
-    
+
     time.sleep(2)
-    
+
     print ('DONE!')
-     
-    
+
+
 def make_driver(context):
     '''
     Создать объект-драйвер для взаимодействия с браузером.
-    Можно вызывать в before_feature, если мы хотим каждый тест проводить, 
-    в отдельном браузере. 
+    Можно вызывать в before_feature, если мы хотим каждый тест проводить,
+    в отдельном браузере.
     P.S. в таком случае, не забыть в after_feature закрывать браузер.
     '''
     # использовать конкретные бинарники
     print ('Opening browser now!')
-    path_to_binary = getcwd() + '/steps/data/firefox/firefox'
-    binary = FirefoxBinary(path_to_binary)
     profile = FirefoxProfile()
-    #profile.native_events_enabled = True
+    # profile.native_events_enabled = True
     profile.set_preference("browser.download.folderList", 2)
     profile.set_preference("browser.download.manager.showWhenStarting", False)
-    #path_to_download_folder = os.path.join(' ', 'download_tests')
-    #path_to_test_folder = os.getcwd()
-    #download_folder_path = path_to_test_folder + path_to_download_folder[1:]
-    profile.set_preference("browser.download.dir", download_folder_path)
-    print (download_folder_path)
+    profile.set_preference("browser.download.dir", os.getcwd())
     print (os.getcwd())
-    profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/x-msdos-program, application/octet-stream, image/png, application/pdf, application/x-pdf, image/svg+xml, application/postscript, text/plain, application/download")
+    profile.set_preference("browser.helperApps.neverAsk.saveToDisk",
+                           "application/x-msdos-program, application/octet-stream, image/png, application/pdf, application/x-pdf, image/svg+xml, application/postscript, text/plain, application/download")
     context.list_ud = list()
     context.stand = STAND
     # выбор профиля
     if "win" in platform:
         context.browser = webdriver.Firefox(firefox_profile=profile)
+    elif "linux" in platform:
+        path_to_binary = "/usr/bin/firefox"
+        binary = FirefoxBinary(path_to_binary)
+        context.browser = webdriver.Firefox(
+            firefox_profile=profile, firefox_binary=binary)
     else:
-        context.browser = webdriver.Firefox(firefox_profile=profile, firefox_binary=binary)
+        path_to_binary = getcwd() + '/steps/data/firefox/firefox'
+        binary = FirefoxBinary(path_to_binary)
+        context.browser = webdriver.Firefox(
+            firefox_profile=profile, firefox_binary=binary)
     context.browser.maximize_window()
     # вызываем логин-фичу
     try:
@@ -107,10 +118,11 @@ def make_driver(context):
         print ('cant login!!!')
         context.browser.quit()
 
+
 def before_step(context, step):
     '''
     Служебная функция behave с говорящим названием.
-    Подробнее на http://pythonhosted.org/behave/. 
+    Подробнее на http://pythonhosted.org/behave/.
     '''
     time.sleep(0.5)
     # текст, который показан, когда на странице есть блок загрузки.
@@ -120,38 +132,43 @@ def before_step(context, step):
     load_page_text = 'Сохранение'
     sleep_while_show_text(context, load_page_text)
 
+
 def before_all(context):
     '''
     Служебная функция behave с говорящим названием.
-    Подробнее на http://pythonhosted.org/behave/. 
+    Подробнее на http://pythonhosted.org/behave/.
     '''
     pass
+
 
 def before_feature(context, feature):
     '''
     Служебная функция behave с говорящим названием.
-    Подробнее на http://pythonhosted.org/behave/. 
+    Подробнее на http://pythonhosted.org/behave/.
     '''
     # запустить браузер для теста
     make_driver(context)
 
 
-def after_scenario(context, scenario):        
+def after_scenario(context, scenario):
     # getcwd()+'/screenshot_failed/'+time.strftime('Date and time: %d/%m/%y %H:%M') + scenario.name +'.png'
-    name_screenshot = 'error in scenario: '+scenario.name+'.png'
+    name_screenshot = 'error in scenario: ' + scenario.name + '.png'
     name_screenshot = name_screenshot.replace(" ", "_")
-    if scenario.status != 'passed': context.browser.get_screenshot_as_file(name_screenshot)
+    if scenario.status != 'passed':
+        context.browser.get_screenshot_as_file(name_screenshot)
+
 
 def after_feature(context, feature):
     '''
     Служебная функция behave с говорящим названием.
-    Подробнее на http://pythonhosted.org/behave/. 
+    Подробнее на http://pythonhosted.org/behave/.
     '''
-    # Вызвать функцию для подготовки данных, которая вызовет api-metod для удаление УД.
-  #  delete_saved_document(context)
+    # Вызвать функцию для подготовки данных,
+    # которая вызовет api-metod для удаление УД.
+    # delete_saved_document(context)
     try:
         context.browser.quit()
-    except: 
+    except:
         print ('Already exit!st')
     # после выполнения теста закрыть браузера
     print ('Browser closes...')
@@ -160,8 +177,7 @@ def after_feature(context, feature):
 def after_all(context):
     '''
     Служебная функция behave с говорящим названием.
-    Подробнее на http://pythonhosted.org/behave/. 
+    Подробнее на http://pythonhosted.org/behave/.
     '''
     pass
- #   context.browser.quit()
-
+    # context.browser.quit()
--- steps/logic/generators.py
+++ steps/logic/generators.py
@@ -1,18 +1,8 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException, TimeoutException, StaleElementReferenceException
-from datetime import datetime, timedelta
-import selenium.webdriver.common.action_chains as AC
-from settings import download_folder_path
+from datetime import datetime
 import time
 import os
-import sys
 from os import listdir
-from os.path import join
 from settings import settings_test as settings
 import random
 now = datetime.today()
@@ -22,120 +12,116 @@ TIME_FOR_WAIT = 30
 SERVER = settings['server']
 
 
-
-
-# открывает файл, записывает данные. Значение file должно быть в скобках  "some.txt"
+# открывает файл, записывает данные.
+# Значение file должно быть в скобках  "some.txt"
 class FileActions(object):
-	# открывает файл, записывает данные. Значение file должно быть в скобках  "some.txt"
-	def write_in_file(self, file, save_text):
-		my_file = open(file, "r+")
-		my_file.write(save_text + "\n")
+    # открывает файл, записывает данные.
+    # Значение file должно быть в скобках  "some.txt"
+    def write_in_file(self, file, save_text):
+        my_file = open(file, "r+")
+        my_file.write(save_text + "\n")
 
-	# Открывает файл и возвращает первую строку
-	def read_file(self, file):
-		my_file = open(file, "r+")
-		fileText = my_file.readline()
-		print ("%s" + " idea created!!!") % fileText
-		my_file.close()
-		return fileText
+    # Открывает файл и возвращает первую строку
+    def read_file(self, file):
+        my_file = open(file, "r+")
+        fileText = my_file.readline()
+        print ("%s" + " idea created!!!") % fileText
+        my_file.close()
+        return fileText
 
-	# удаляет файл по пути
-	def delete_file(self, file):
-		if file == 'icon8 app':
-			pathToSetup = os.path.join(' ', 'Icons8Setup.exe')
-			file_path = download_folder_path + pathToSetup[1:]
-			print (file_path)
-			os.remove(file_path)
-			try:
-				os.remove(file_path)
-			except WindowsError:
-				pass
+    # удаляет файл по пути
+    def delete_file(self, file):
+        if file == 'icon8 app':
+            pathToSetup = os.path.join(
+                ' ', 'icon8', 'icons8', 'Icons8Setup.exe')
+            os.remove(pathToSetup[1:])
+            try:
+                pathToPart = os.path.join(
+                    ' ', 'icon8', 'icons8', 'Icons8Setup.exe.part')
+                os.remove(pathToPart[1:])
+            except WindowsError:
+                pass
 
-	# Удаляет все файлы с окончание или расширением (extension)
-	def del_by_extension(self, extension):
-		list_of_all_files = listdir(download_folder_path)
-		elements_count = 0
-		for item in list_of_all_files:
-			if item.endswith(extension):
-				elements_count += 1
-				os.remove(os.path.join(download_folder_path, item))
-		assert elements_count > 0
+    # Удаляет все файлы с окончание или расширением (extension)
+    def del_by_extension(self, extension):
+        dir = os.path.join(' ', 'icon8', 'icons8')
+        test = listdir(dir[1:])
+        elements_count = 0
+        for item in test:
+            if item.endswith(extension):
+                elements_count += 1
+                os.remove(os.path.join(dir[1:], item))
+        assert elements_count > 0
 
-	# Ждёт пока не исщезнит файл .part
-	def downloading_file(self, extension):
-		list_of_all_files = listdir(download_folder_path)
-		elements_count = 0
-		for item in list_of_all_files:
-			if item.endswith(extension):
-				time_waited = 0
-				elements_count += 1
-				download_end = False
-				while download_end == False:
-					path = os.path.exists(os.path.join(download_folder_path, item))
-					if path == True:
-						time.sleep(5)
-						time_waited += 5
-					elif time_waited == 240:
-						print ('download is to long')
-						break
-					elif path == False:
-						print ('Donwload file with "' + extension + '" extension ended')
-						download_end = True
-				print (str(time_waited) + "sec download")
-		assert elements_count > 0
-
-
+    # Ждёт пока не исщезнит файл .part
+    def downloading_file(self, extension):
+        dir = os.path.join(' ', 'icon8', 'icons8')
+        test = listdir(dir[1:])
+        elements_count = 0
+        for item in test:
+            if item.endswith(extension):
+                time_waited = 0
+                elements_count += 1
+                download_end = False
+                while download_end == False:
+                    path = os.path.exists(os.path.join(dir[1:], item))
+                    if path:
+                        time.sleep(5)
+                        time_waited += 5
+                    elif time_waited == 240:
+                        print ('download is to long')
+                        break
+                    elif not path:
+                        print ('Donwload file with "' +
+                               extension + '" extension ended')
+                        download_end = True
+                print (str(time_waited) + "sec download")
+        assert elements_count > 0
 
 
 class RandomGenerate(object):
-	def random_betweenValue(self, first_value, last_value):
-		return random.randint(first_value, last_value)
+    def random_betweenValue(self, first_value, last_value):
+        return random.randint(first_value, last_value)
 
-	# Возвращает случайное значение из списка
-	def random_listValue(self, list):
-		return random.choice(list)
+    # Возвращает случайное значение из списка
+    def random_listValue(self, list):
+        return random.choice(list)
 
-	# Генерирует рандомное идею из 6 значений из списка и сохраняет его в файл
-	def random_idea_name(self):
-		idea = ''
-		for x in range(6):
-			idea = idea + random.choice(list('qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'))
-		pathToFile = os.path.join('steps', 'logic', 'ideas.txt')
-		print (pathToFile + "path to idea file")
-		Fileactions = FileActions()
-		Fileactions.write_in_file(pathToFile, idea)  # "steps\logic\ideas.txt"
-		return idea
+    # Генерирует рандомное идею из 6 значений из списка и сохраняет его в файл
+    def random_idea_name(self):
+        idea = ''
+        symbols = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+        for x in range(6):
+            idea = idea + \
+                random.choice(
+                    list(symbols))
+        pathToFile = os.path.join('steps', 'logic', 'ideas.txt')
+        print (pathToFile + "path to idea file")
+        Fileactions = FileActions()
+        Fileactions.write_in_file(pathToFile, idea)  # "steps\logic\ideas.txt"
+        return idea
 
-	# Генерирует рандомный текст
-	def random_text(self, number_of_testItems):
-		text = ''
-		for x in range(number_of_testItems):
-			text = text + random.choice(list('qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890'))
-		return text
+    # Генерирует рандомный текст
+    def random_text(self, number_of_testItems):
+        text = ''
+        symbols = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890'
+        for x in range(number_of_testItems):
+            text = text + \
+                random.choice(
+                    list(symbols))
+        return text
 
-	# Генерирует рандомное имейл из 6 значений из списка и + '@gmail.com'
-	def random_email(self):
-		email = ''
-		for x in range(6):
-			email = email + random.choice(list('qwertyuiopasdfghjklzxcvbnm'))
-		return email + '@gmail.com'
-
-
-
-
-
+    # Генерирует рандомное имейл из 6 значений из списка и + '@gmail.com'
+    def random_email(self):
+        email = ''
+        for x in range(6):
+            email = email + random.choice(list('qwertyuiopasdfghjklzxcvbnm'))
+        return email + '@gmail.com'
 
 
 class ValueGenerate(object):
-	# Генерирует значения промежуточные значения между first_value и second_value
-	def values_in_range(self, first_value, second_value):
-		for values in range(first_value, second_value):
-			return values
-
-
-
-
-
-
-
-
+    # Генерирует значения промежуточные значения
+    # между first_value и second_value
+    def values_in_range(self, first_value, second_value):
+        for values in range(first_value, second_value):
+            return values
